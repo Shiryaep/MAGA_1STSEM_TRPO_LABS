3a76b92a0db76940b5cdb27807aa0aa6a8c4d005
Shiryaev <shiryaev_pi@nppgamma.ru>
2022-12-24 14:40:57 +0300

    Результат изменений в лабе 1

--- db_functions.cpp
+++ db_functions.cpp
@@ -1,42 +1,44 @@
 #ifndef __DB__CPP__CLASS__
 #define __DB__CPP__CLASS__
 #include "db_header.hpp"
-#include <errno.h>
 #include <fstream>
-template<typename KeyType, typename ValType>
-ValType FILEDB<KeyType, ValType>::read_one(KeyType key) {
+template<typename keyT, typename valueT>
+valueT FILEDB<keyT, valueT>::read_one(keyT key) 
+{
     std::ifstream ifile(this->dbfilename);
     std::string ikey, ival, nstr;
-    while (std::getline(ifile, ikey, '=')) {
-        if (ikey[0] == '\n'){
-            ikey.erase(ikey.begin());
-        }
-        // std::strstream strs(nstr);
-        // strstr
-        // strstr = std::strstream(nstr);
+    while (std::getline(ifile, ikey, '=')) 
+    {
+        if (ikey[0] == '\n')
+        { ikey.erase(ikey.begin()); }
         std::getline(ifile, ival);
-        if (std::string(key) == ikey) {
+        if (std::string(key) == ikey) 
+        {   
             ifile.close();
-            return ValType(ival);
+            return valueT(ival);
         }
     }
     ifile.close();
     return "";
 }
-template<typename KeyType, typename ValType>
-int FILEDB<KeyType, ValType>::write(KeyType key, ValType value) {
+template<typename keyT, typename valueT>
+int FILEDB<keyT, valueT>::write(keyT key, valueT value) 
+{
     std::ofstream ofile;
     ofile.open(this->dbfilename, std::ios::app);
-    if (!ofile) {
+    if (!ofile) 
+    {
         std::cout << strerror(errno) << " ";
         return -1;
     }
-    if (this->read_one(key) != ValType()) {
+    if (this->read_one(key) != valueT()) 
+    {
         ofile.close();
         return -1;
     }
     ofile << std::endl << std::string(key) << "=" << std::string(value);
-    if (ofile.bad()) {
+    if (ofile.bad()) 
+    {
         ofile.close();
         return -1;
     }
@@ -44,13 +46,16 @@ int FILEDB<KeyType, ValType>::write(KeyType key, ValType value) {
 
     return 0;
 }
-template<typename KeyType, typename ValType>
-std::map<KeyType, ValType>* FILEDB<KeyType, ValType>::read_all() {
-    std::map<KeyType, ValType> *res = new std::map<KeyType, ValType>;
+template<typename keyT, typename valueT>
+std::map<keyT, valueT>* FILEDB<keyT, valueT>::read_all() 
+{
+    std::map<keyT, valueT> *res = new std::map<keyT, valueT>;
     std::ifstream ifile(this->dbfilename);
     std::string ikey, ival;
-    while (std::getline(ifile, ikey, '=')) {
-        if (ikey[0] == '\n'){
+    while (std::getline(ifile, ikey, '=')) 
+    {
+        if (ikey[0] == '\n')
+        {
             ikey.erase(ikey.begin());
         }
         std::getline(ifile, ival);
@@ -59,19 +64,23 @@ std::map<KeyType, ValType>* FILEDB<KeyType, ValType>::read_all() {
     ifile.close();
     return res;
 }
-template<typename KeyType, typename ValType>
-int FILEDB<KeyType, ValType>::erase(KeyType key) {
+template<typename keyT, typename valueT>
+int FILEDB<keyT, valueT>::erase(keyT key) 
+{
     std::fstream ofile;
     // get all keys from file
-    std::map<KeyType, ValType>* res = this->read_all();
+    std::map<keyT, valueT>* res = this->read_all();
     ofile.open(this->dbfilename, std::ios::out);
-    if (!ofile) {
+    if (!ofile) 
+    {
         std::cout << strerror(errno) << " ";
         return -1;
     }
     // Write all keys to file exept "key"
-    for (auto [ ikey, ival ] : *res) {
-        if (key != ikey){
+    for (auto [ ikey, ival ] : *res) 
+    {
+        if (key != ikey)
+        {
             #ifdef DEBUG
             std::cout << ikey << ": " << ival << "<br>"; //DEBUG
             #endif
@@ -79,20 +88,20 @@ int FILEDB<KeyType, ValType>::erase(KeyType key) {
         }
     }
     delete res;
-    if (ofile.bad()) {
+    if (ofile.bad()) 
+    {
         return -1;
     }
     ofile.close();
 
     return 0;
 }
-template<typename KeyType, typename ValType>
-FILEDB<KeyType, ValType>::FILEDB() {
-    this->dbfilename = "db.txt";
-}
-template<typename KeyType, typename ValType>
-FILEDB<KeyType, ValType>::~FILEDB() {
-    return;
-}
+template<typename keyT, typename valueT>
+FILEDB<keyT, valueT>::FILEDB() 
+{   this->dbfilename = "db.txt"; }
+
+template<typename keyT, typename valueT>
+FILEDB<keyT, valueT>::~FILEDB() 
+{   return; }
 
 #endif
--- db_header.hpp
+++ db_header.hpp
@@ -9,22 +9,26 @@
 добавление записи, удаление, просмотр списка, просмотр одной записи.
 Записи в базе данных хранятся построчно в файле, а отдельные поля записи разделены символами-разделителями.
 */
-template<typename KeyType, typename ValType>
-class FILEDB{
+template<typename keyT, typename valueT>
+class FILEDB
+{
     public:
+    // конструкторы
     FILEDB();
     FILEDB(std::string filename);
+
     ~FILEDB();
 
-    // write record to db file
-    int write(KeyType key, ValType value);
-    // read one record from db file
-    ValType read_one(KeyType key);
-    // read all records from db file
-    std::map<KeyType, ValType>* read_all();
-    // erase record from db file
-    int erase(KeyType key);
+    // записать строчку в базу данных
+    int write(keyT key, valueT value);
+    // считать строку из базы данных по ключу
+    valueT read_one(keyT key);
+    // считать все из базы даных
+    std::map<keyT, valueT>* read_all();
+    // стереть строку из базы данных по ключу
+    int erase(keyT key);
     private:
+    // название файла с базой данных
     std::string dbfilename;
 };
 
